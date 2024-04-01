7afb3d7c129b421c37bafa1e3d9fe039efc2bc5f
Benjamin Kraemer <benjaminkraemer40@gmail.com>
2024-04-01 01:49:05 -0700

    Fixes scene change for main menu

--- Millennium Line/Assets/MenuItems/MainMenu/MainMenu.cs
+++ Millennium Line/Assets/MenuItems/MainMenu/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Scene1");
     }
 
     public void QuitGame(){
